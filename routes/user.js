7f5325070643a72591f6bbaf39b936d2c22227cc
drivadosayan <techsupport1@drivado.com>
2024-02-21 18:59:43 +0530

    auth middleware

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { registerUser, login, getAccessToken, authUser, getSingleUserDetailsWithId, getAllUsers } = require("../controllers/userController");
+const auth = require("../middlewares/auth");
 const router = express.Router();
 
 
 router.route("/registerUser").post(registerUser)
 router.route("/login").post(login)
 router.route("/access_Token").get(getAccessToken);
-router.route("/authUser").get(authUser);
+router.route("/authUser").get(auth,authUser);
 router.route("/getSingleUserDetailsWithId").get(getSingleUserDetailsWithId);
 router.route("/getAllUsers").get(getAllUsers);
 
